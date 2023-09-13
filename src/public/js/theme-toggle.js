5e882ebcbcdae2736a82cf7efbec252185f944f6
DanielZ1337 <danielz2nd@hotmail.com>
2023-09-13 17:19:38 +0200

    theme-toggle

--- src/public/js/theme-toggle.js
+++ src/public/js/theme-toggle.js
@@ -9,9 +9,63 @@ if (theme) {
 }
 
 const button = document.getElementById('theme-toggle')
-const handleThemeToggle = () => {
-    html.className = html.className === 'dark' ? 'light' : 'dark'
-    localStorage.setItem("theme", html.className);
+const moon = document.getElementById('theme-toggle-moon')
+const sun = document.getElementById('theme-toggle-sun')
+
+switch (html.className) {
+    case 'dark':
+        moon.style.display = 'none'
+        sun.style.display = 'block'
+        break;
+    case 'light':
+        moon.style.display = 'block'
+        sun.style.display = 'none'
+        break;
+    default:
+        moon.style.display = 'block'
+        sun.style.display = 'none'
 }
 
-button.addEventListener('click', handleThemeToggle)
+button.addEventListener('click', () => {
+    html.className = html.className === 'dark' ? 'light' : 'dark'
+    localStorage.setItem("theme", html.className);
+    switch (html.className) {
+        case 'dark':
+            anime({
+                targets: moon,
+                opacity: 0,
+                complete: function() {
+                    moon.style.display = 'none'
+                    anime({
+                        targets: sun,
+                        opacity: 100,
+                        complete: function() {
+                            sun.style.display = 'block'
+                        }
+                    })
+                }
+            })
+
+            break;
+        case 'light':
+            anime({
+                targets: moon,
+                opacity: 100,
+                complete: function() {
+                    moon.style.display = 'block'
+                    anime({
+                        targets: sun,
+                        opacity: 0,
+                        complete: function() {
+                            sun.style.display = 'none'
+                        }
+                    })
+                }
+            })
+            break;
+        default:
+            moon.style.display = 'block'
+            sun.style.display = 'none'
+            break;
+    }
+})
--- src/resources/views/comps/theme-toggle.blade.php
+++ src/resources/views/comps/theme-toggle.blade.php
@@ -1,5 +1,7 @@
 @section('theme-toggle')
     <button id="theme-toggle">
-        Change theme
+        <img src="{{asset('icons/sun.svg')}}" alt="sun" id="theme-toggle-sun">
+        <img src="{{asset('icons/moon.svg')}}" alt="moon" id="theme-toggle-moon">
     </button>
+    <script src="{{asset('js/anime.min.js')}}"></script>
     <script src="{{asset('js/theme-toggle.js')}}"></script>

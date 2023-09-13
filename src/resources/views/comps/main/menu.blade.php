4d1b18069824fe2f796482f32e99ac9c8e11875d
David Nielsen <davni22@student.sdu.dk>
2023-09-13 09:11:09 +0200

    :)

---
+++ src/resources/views/comps/main/menu.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+    <body>
+        @section('menu')
+            <link href="{{asset('css/app.css')}}" rel="stylesheet">
+            <div class="menu">
+                <a href="/  "><img src="/favicon.ico"></a>
+            </div>
+    </body>
+</html>
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -9,7 +9,12 @@
     <body>
         <!-- Header imported from ./comps/main/header.blade.php -->
         @include('comps.main.header')
-        
+        <!-- Menu imported from ./comps/main/menu.blade.php -->
+        @include('comps.main.menu')
+        <!-- Main Body Start -->
+
+
+        <!-- Main Body End -->
         <!-- Footer imported from ./comps/main/footer.blade.php -->
         @include('comps.main.footer')
     </body>

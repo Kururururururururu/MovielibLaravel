350986057232c81111264d572d1cd9ef1fc31e74
Landersen <lukasandersen15@gmail.com>
2023-09-24 11:53:19 +0200

    changes

--- src/resources/views/register.blade.php
+++ src/resources/views/register.blade.php
@@ -13,6 +13,18 @@
     <div class="container">
         <h1>Register</h1>
     </div>
+    <form action="/register">
+        <div class="form-group">
+            <label for="first_name">First name:</label>
+            <input type="text" id="First name" name="First name" required><br>
+        </div>
+        <div action="form-group">
+        <label for="last_name">Last name:</label>
+        <input type="text" id="Last name" name="Last name" required><br>
+        </div>
+        <input type="register" value="Register">
+        <input type="submit" value="Login">
+    </form>
     
 </body>
 </html>

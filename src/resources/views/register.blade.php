ce4c0db3dcd7305236140bcf6cd39360423991cd
Landersen <lukasandersen15@gmail.com>
2023-09-24 14:23:47 +0200

    register done

--- src/resources/views/register.blade.php
+++ src/resources/views/register.blade.php
@@ -11,19 +11,23 @@
      <!-- Header imported from ./comps/main/header.blade.php -->
      @include('comps.main.header')
     <div class="container">
-        <h1>Register</h1>
+        <h1>Movie Rater</h1>
     </div>
     <form action="/register">
+        <label for="name">Name:</label>
         <div class="form-group">
-            <label for="first_name">First name:</label>
-            <input type="text" id="First name" name="First name" required><br>
+            <input type="text" id="First name" name="First name" placeholder="First name" required><br>
+            <input type="text" id="Last name" name="Last name" placeholder="Last name" required><br>
         </div>
-        <div action="form-group">
-        <label for="last_name">Last name:</label>
-        <input type="text" id="Last name" name="Last name" required><br>
+        <div>
+            <label for="email">Email:</label>
+            <input type="email" id="Email" name="Email" required><br>
+            <label for="password">Password:</label>
+            <input type="password" id="Password" name="Password" required><br>
+            <label for="password_confirmation">Confirm password:</label>
+            <input type="password" id="Password confirmation" name="Password confirmation" required><br>
         </div>
-        <input type="register" value="Register">
-        <input type="submit" value="Login">
+        <input type="register" value="Create Movie Rater account">
     </form>
     
 </body>

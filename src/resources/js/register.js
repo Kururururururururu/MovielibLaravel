042d6bd1307502093ba0246cd0542645fe64033f
Landersen <lukasandersen15@gmail.com>
2023-09-24 17:01:46 +0200

    d

---
+++ src/resources/js/register.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const signinButton = document.getElementById("signinButton");
+  
+    signinButton.addEventListener("click", function(event) {
+      event.preventDefault(); // Prevent the form from submitting
+  
+      // Add your custom logic here for what should happen when the "Sign in" button is clicked
+      // For example, you can redirect the user to the login page or show a login modal.
+      // You can also remove this entire event listener if you don't need any special behavior.
+    });
+  });
--- src/resources/views/register.blade.php
+++ src/resources/views/register.blade.php
@@ -5,6 +5,7 @@
     <link href="{{ asset('css/css-reset.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/register.css') }}" rel="stylesheet">
+    <script> src="register.js"</script>
 </head>
 
 <body>
@@ -14,20 +15,21 @@
         <h1>Movie Rater</h1>
     </div>
     <form action="/register">
-        <label for="name">Name:</label>
-        <div class="form-group">
-            <input type="text" id="First name" name="First name" placeholder="First name" required><br>
-            <input type="text" id="Last name" name="Last name" placeholder="Last name" required><br>
-        </div>
         <div>
+            <label for="name">Name:</label>
+            <input type="text" id="username" name="username" placeholder="First and last name" required><br>
             <label for="email">Email:</label>
-            <input type="email" id="Email" name="Email" required><br>
+            <input type="email" id="Email" name="Email"required><br>
             <label for="password">Password:</label>
             <input type="password" id="Password" name="Password" required><br>
             <label for="password_confirmation">Confirm password:</label>
             <input type="password" id="Password confirmation" name="Password confirmation" required><br>
         </div>
-        <input type="register" value="Create Movie Rater account">
+        <input type="submit" value="Create Movie Rater account">
+        <div class="Alreadycreated">
+        <label for="Accountexist">Already have an account?</label>
+        <button class="link" id="signinButton">Sign in</button>
+        </div>
     </form>
     
 </body>

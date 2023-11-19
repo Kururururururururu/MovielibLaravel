88fecb566edcfd0949f94f1c12c4dfe857cdf468
Benji0106 <112694300+Benji0106@users.noreply.github.com>
2023-11-19 12:48:16 +0100

    Update watchlist.blade.php

--- src/resources/views/watchlist.blade.php
+++ src/resources/views/watchlist.blade.php
@@ -13,9 +13,34 @@
         rel="stylesheet">
 </head>
 <body>
-    @include('comps.main.header')
-    @include('comps.main.menu')
-    here
-    
+    <Header>
+        @include('comps.main.header')
+    </Header>
+    <Nav>
+        
+    </Nav>
+    <section class="movie-list">
+        @foreach ($movies-> results as $movie)
+            <a class="movie-link" href="{{"/movie?id=" . $movie->id}}">
+                <div class="movie-card">
+                    <img class="movie-image" src="{{
+                    @getimagesize("https://image.tmdb.org/t/p/w200/" . $movie->poster_path) ?
+                    "https://image.tmdb.org/t/p/w200/" . $movie->poster_path :
+                    asset('icons/movie_fallback_image.jpg')
+                    }}">
+                    <h2 class="movie-text">{{ $movie->title }}</h2>
+                    <!-- Make either star or text based rating display here. 
+                    Use own database instead of tmdb data. -->
+                    <p class="movie-rating">{{$movie->vote_average}}</p>
+                    <p class="movie-release">{{$movie->release_date}}</p>
+                </div>
+                <div class="page-select">
+                    
+                </div>
+            </a>
+        @endforeach
+    </section>
+    <footer>
 
+    </footer>
 </body>

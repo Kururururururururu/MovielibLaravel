6934e3f54f4344ef417f243e4500f37d7e075b28
DanielZ1337 <danielz2nd@hotmail.com>
2023-11-24 17:04:00 +0100

    Update vite.config.js

--- src/vite.config.js
+++ src/vite.config.js
@@ -5,7 +5,30 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
     plugins: [
         laravel({
-            input: ["public/css/app.css", "public/js/app.jsx"],
+            input: [
+                "public/css/app.css",
+                "public/js/app.jsx",
+                "public/js/register.js",
+                "public/js/pages/index.jsx",
+                "public/js/pages/login.jsx",
+                "public/js/pages/register.jsx",
+                "public/js/pages/movie.index/movie.index.jsx",
+                "public/js/pages/movies.index/movies.index.jsx",
+                "public/js/pages/movie.index/contants.js",
+                "public/js/pages/movie.index/movie-comment.jsx",
+                "public/js/pages/movie.index/movie-comments-context.jsx",
+                "public/js/pages/movie.index/movie-comments-section.jsx",
+                "public/js/pages/movie.index/movie-header.jsx",
+                "public/js/pages/movie.index/movie-info-item.jsx",
+                "public/js/pages/movie.index/movie-pill.jsx",
+                "public/js/pages/movie.index/movie-poster.jsx",
+                "public/js/pages/movie.index/movie-tab-button.jsx",
+                "public/js/pages/movie.index/movie-tab-casts-section.jsx",
+                "public/js/pages/movie.index/movie-tab-comments-section.jsx",
+                "public/js/pages/movie.index/movie-tab-section.jsx",
+                "public/js/pages/movie.index/movie-tab.jsx",
+                "public/js/pages/movie.index/movie-watchlist-button.jsx",
+            ],
             refresh: true,
         }),
         {

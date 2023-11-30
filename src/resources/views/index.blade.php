8b107f4cd4f1e83c50c1168ccafe4b6ba9b841a0
DanielZ1337 <danielz2nd@hotmail.com>
2023-11-30 01:24:42 +0100

    Update index.blade.php

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -13,7 +13,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
         rel="stylesheet">
     @viteReactRefresh
-    @vite(['public/js/app.jsx', 'public/css/app.css/', 'public/js/pages/index.jsx'])
+    @vite(['public/js/app.jsx', 'public/css/app.css', 'public/js/pages/index.jsx'])
 </head>
 
 <body>

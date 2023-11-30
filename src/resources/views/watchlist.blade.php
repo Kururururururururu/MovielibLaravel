a4985d15dd1bde0f8d4bdcc910e098552155b488
Benji0106 <112694300+Benji0106@users.noreply.github.com>
2023-11-30 12:44:23 +0100

    nu med CSS & JS og lidt mere

--- src/resources/views/watchlist.blade.php
+++ src/resources/views/watchlist.blade.php
@@ -2,47 +2,37 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
+    <!-- Title -->
     <title>Watchlist</title>
+
     <!-- Styles -->
     <link href="{{ asset('css/css-reset.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"
-        rel="stylesheet">
+    <link href="{{ asset('css/watchlist.css') }}" rel="stylesheet">
+
+    <!-- Javascript -->
+    <script src="{{ asset('js/watchlist.js') }}"></script>
+
 </head>
+
 <body>
-    <Header>
+    <!-- Header imported from ./comps/main/header.blade.php -->
+    <header>
         @include('comps.main.header')
-    </Header>
-    <Nav>
-
-    </Nav>
-    <section class="movie-list">
-        
-        @foreach ($movies-> results as $movie)
-            <a class="movie-link" href="{{"/movie?id=" . $movie->id}}">
-                <div class="movie-card">
-                    <img class="movie-image" src="{{
-                    @getimagesize("https://image.tmdb.org/t/p/w200/" . $movie->poster_path) ?
-                    "https://image.tmdb.org/t/p/w200/" . $movie->poster_path :
-                    asset('icons/movie_fallback_image.jpg')
-                    }}">
-                    <h2 class="movie-text">{{ $movie->title }}</h2>
-                    <!-- Make either star or text based rating display here. 
-                    Use own database instead of tmdb data. -->
-                    <p class="movie-rating">{{$movie->vote_average}}</p>
-                    <p class="movie-release">{{$movie->release_date}}</p>
-                </div>
-                <div class="page-select">
-                    
-                </div>
-            </a>
-        @endforeach
+    </header>
     
+    <!-- Main section -->
+    <section class="movie-list">
+        <h1>Hello there!</h1>
     </section>
-    <footer>
 
-    </footer>
+
+    <!--
+    PLAN:
+    - get user id
+    - get movie id's related to user id form the database
+    - get details about the movies from the API
+    - show movies
+    -->
+    
 </body>

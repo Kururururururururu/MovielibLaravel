e519f04ebd14cc1fbd4cacc4636c34ca3be0a8d3
jonaslissner <jolis21@student.sdu.dk>
2023-12-05 19:44:42 +0100

    setup for individual extension

--- src/app/Http/Controllers/IndexController.php
+++ src/app/Http/Controllers/IndexController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
 class IndexController extends Controller

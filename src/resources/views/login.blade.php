017d896f303593ede0523cb6da053a74abd733d6
Landersen <lande22@student.sdu.dk>
2023-12-06 10:28:35 +0100

    Removed my part

--- src/resources/views/login.blade.php
+++ src/resources/views/login.blade.php
@@ -1,9 +1,7 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Login</title>
     <!-- Styles -->
     <link href="{{ asset('css/css-reset.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
@@ -15,24 +13,22 @@
 </head>
 
 <body>
-    <header>
-        <!-- Header imported from ./comps/main/header.blade.php -->
-        @include('comps.main.header')
-    </header>
-    <main>
-        <form method="POST" action="{{ route('login') }}" class="loginform">
-            @csrf
-            <h1>Login</h1>
-            <pre>
-                
-            </pre>
-            <label for="username">Username:</label>
-            <input type="text" id="username" name="username" required><br>
-            <label for="password">Password:</label>
-            <input type="password" id="password" name="password" required><br>
-            <input type="submit" id="registerbutton" value="Register">
-            <input type="submit" id="loginbutton" value="Login">
-        </form>
-    </main>
+     <!-- Header imported from ./comps/main/header.blade.php -->
+     @include('comps.main.header')
+    <div class="lort">
+    <form method="POST" action="{{ route('login') }}" class="loginform">
+    @csrf
+        <h1>Login</h1>
+        <pre>
+            
+        </pre>
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required><br>
+        <label for="password">Password:</label>
+        <input type="password" id="password" name="password" required><br>
+        <input type="submit" id="registerbutton" value="Register">
+        <input type="submit" id="loginbutton" value="Login">
+    </form>
+    </div>
 </body>
 </html>

f961fe6fa20efdd7104ccf4d659eed1bf92694d6
jonaslissner <jolis21@student.sdu.dk>
2023-12-06 21:03:50 +0100

    added autoscroll functionality

--- src/public/js/carousel.js
+++ src/public/js/carousel.js
@@ -1,8 +1,43 @@
-const initSlider = () => {
+const initCarousel = () => {
     const moviesList = document.querySelector(".carousel-wrapper .movieCarousel");
     const slideButtons = document.querySelectorAll(".carousel-wrapper .slide-button");
-    const maxScroll = moviesList.scrollWidth;
+    const nextButton = Array.from(slideButtons).find(button => button.id === "next-slide");
 
+    const maxScroll = moviesList.scrollWidth;
+    const scrollLength = 1.20;
+
+    let autoScrollInterval;
+
+    const restartAutoScroll = () => {
+        clearInterval(autoScrollInterval);
+        autoScrollInterval = setInterval(() => {
+            console.log("Auto-Scrolling...")
+            nextButton.click();
+        }, 8000); // Auto-scroll every 8 seconds
+    };
+
+    moviesList.addEventListener("wheel", restartAutoScroll);
+    moviesList.addEventListener("mouseover", () => clearInterval(autoScrollInterval));
+    moviesList.addEventListener("mouseout", restartAutoScroll);
+
+    slideButtons.forEach(button => {
+        button.addEventListener("click", () =>{
+            const direction = button.id === "previous-slide" ? -1 : 1;
+
+            if(moviesList.scrollLeft + moviesList.clientWidth == maxScroll && button.id === "next-slide"){
+                moviesList.scrollBy({left: -(moviesList.scrollLeft + moviesList.clientWidth), behavior: "smooth"});
+            } else if(moviesList.scrollLeft == 0 && button.id === "previous-slide") {
+                moviesList.scrollBy({left: maxScroll, behavior: "smooth"});
+            } else {
+                const scrollAmount = (moviesList.clientWidth)/scrollLength * direction;
+                moviesList.scrollBy({ left: scrollAmount, behavior: "smooth" });
+            }
+
+            restartAutoScroll();
+        });
+    });
+
+    restartAutoScroll();
     /*moviesList.addEventListener("scroll", (event) => {
         moviesList.addEventListener("wheel", (event) => {
 
@@ -16,24 +51,6 @@ const initSlider = () => {
         }
         })
     })*/
-
-
-
-    slideButtons.forEach(button => {
-        button.addEventListener("click", () =>{
-            const direction = button.id === "previous-slide" ? -1 : 1;
-            console.log("Scroll width: " + maxScroll)
-            if(moviesList.scrollLeft + moviesList.clientWidth == maxScroll && button.id === "next-slide"){
-                console.log("End reached!")
-                moviesList.scrollBy({left: -(moviesList.scrollLeft + moviesList.clientWidth), behavior: "smooth"});
-            } else if(moviesList.scrollLeft == 0 && button.id === "previous-slide") {
-                moviesList.scrollBy({left: maxScroll, behavior: "smooth"});
-            } else {
-                const scrollAmount = (moviesList.clientWidth)/1.20 * direction;
-                moviesList.scrollBy({ left: scrollAmount, behavior: "smooth" });
-            }
-        });
-    });
 }
 
-window.addEventListener("load", initSlider)
+window.addEventListener("load", initCarousel)
